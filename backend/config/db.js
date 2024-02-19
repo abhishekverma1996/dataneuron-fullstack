d2604e8ce8f45bd2c5e84d94f4c35c1fb7c1d009
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2024-02-19 13:39:43 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,15 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 mongoose.set('strictQuery', false);
 
-const MONGO_URL = process.env.MONGO_URL;
-
-const connection = mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch(error => {
-    console.error('Error connecting to MongoDB:', error.message);
-  });
+const connection = mongoose.connect(process.env.MONGO_URL);
 
 module.exports = {
 	connection
